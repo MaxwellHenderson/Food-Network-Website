c54cbee77c943be988757f22ac8257c2bc20d8fa
Jerry Zhu <jerry.zhu@bellevuecollege.edu>
2019-11-25 13:55:45 -0800

    Backend API calls added

--- client/src/App.js
+++ client/src/App.js
@@ -1,88 +1,89 @@
-'use strict';
+import React, { Component } from "react";
+import NavBar from "./NavBar.js";
+import SideBar from "./SideBar.js";
+import CardList from "./CardList.js";
 
-import React, {Component} from 'react';
-import NavBar from './NavBar.js';
-import SideBar from './SideBar.js';
-import CardList from './CardList.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/style.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/style.css';
-
-class App extends Component{
+class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       foodItems: [
         {
-          name: 'Sushi',
-          imgUrl: 'imgs/makisushi.jpg'
+          name: "Sushi",
+          imgUrl: "imgs/makisushi.jpg"
         },
         {
-          name: 'Pizzatime',
-          imgUrl: 'imgs/itsapizza.jpg'
+          name: "Pizzatime",
+          imgUrl: "imgs/itsapizza.jpg"
         },
         {
-          name: 'Sausages',
-          imgUrl: 'imgs/Sausages_large.jpg'
+          name: "Sausages",
+          imgUrl: "imgs/Sausages_large.jpg"
         },
         {
-          name: 'Lasagna',
-          imgUrl: 'imgs/lasagna.jpg'
+          name: "Lasagna",
+          imgUrl: "imgs/lasagna.jpg"
         },
         {
-          name: 'Mondays',
-          imgUrl: 'imgs/lasagna.jpg'
+          name: "Mondays",
+          imgUrl: "imgs/lasagna.jpg"
         },
         {
-          name: 'Taco Tuesday!',
-          imgUrl: 'imgs/tacos.jpg'
-        },  
-        {
-          name: 'Mmmmmm, Ramen!',
-          imgUrl: 'imgs/ramen.jpg'
+          name: "Taco Tuesday!",
+          imgUrl: "imgs/tacos.jpg"
         },
         {
-            name: 'Sushi',
-          imgUrl: 'imgs/makisushi.jpg'
+          name: "Mmmmmm, Ramen!",
+          imgUrl: "imgs/ramen.jpg"
         },
         {
-          name: 'Not pho',
-          imgUrl: 'imgs/ramen.jpg'
+          name: "Sushi",
+          imgUrl: "imgs/makisushi.jpg"
         },
         {
-          name: 'Sushi',
-          imgUrl: 'imgs/makisushi.jpg'
+          name: "Not pho",
+          imgUrl: "imgs/ramen.jpg"
         },
         {
-          name: 'Give me the sauce',
-          imgUrl: 'imgs/Sausages_large.jpg'
+          name: "Sushi",
+          imgUrl: "imgs/makisushi.jpg"
         },
         {
-          name: 'Pizza',
-          imgUrl: 'imgs/itsapizza.jpg'
-        }
-        ,
-        {
-          name: 'Taco Tuesday!',
-          imgUrl: 'imgs/tacos.jpg'
-        },  
-        {
-          name: 'Mmmmmm, Ramen!',
-          imgUrl: 'imgs/ramen.jpg'
+          name: "Give me the sauce",
+          imgUrl: "imgs/Sausages_large.jpg"
         },
         {
-            name: 'Sushi',
-          imgUrl: 'imgs/makisushi.jpg'
+          name: "Pizza",
+          imgUrl: "imgs/itsapizza.jpg"
         },
         {
-          name: 'Not pho',
-          imgUrl: 'imgs/ramen.jpg'
+          name: "Taco Tuesday!",
+          imgUrl: "imgs/tacos.jpg"
+        },
+        {
+          name: "Mmmmmm, Ramen!",
+          imgUrl: "imgs/ramen.jpg"
+        },
+        {
+          name: "Sushi",
+          imgUrl: "imgs/makisushi.jpg"
+        },
+        {
+          name: "Not pho",
+          imgUrl: "imgs/ramen.jpg"
         }
       ]
-    }
+    };
   }
 
+  /* Lifecycle hooks */
+  componentDidMount() {
+    this.handleGetMeal();
+  }
 
   render() {
     return (
@@ -93,6 +94,28 @@ class App extends Component{
       </div>
     );
   }
+
+  handleGetMeal = async () => {
+    const url = "/listings";
+    let request = new Request(url, {
+      method: "GET",
+      headers: new Headers()
+    });
+    this.callBackendAPI(request)
+      .then(response => this.setState({ foodItems: response }))
+      .catch(err => console.log(err));
+  };
+
+  /* Fetches our route from the Express server */
+  callBackendAPI = async request => {
+    const response = await fetch(request);
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    return body;
+  };
 }
 
 export default App;
--- client/src/CardList.js
+++ client/src/CardList.js
@@ -1,45 +1,56 @@
 "use strict";
 
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
 
 class CardList extends Component {
-    render() {
-        let foodCards = this.props.foodItems.map((foodItem, index) => {
-            let card = <FoodCard name={foodItem.name} imgUrl={foodItem.imgUrl} imgAlt={foodItem.imgAlt} />
+  render() {
+    console.log(this.props.foodItems);
+    let foodCards = this.props.foodItems.map((foodItem, index) => {
+      let card = (
+        <FoodCard
+          // name={foodItem.name}
+          // imgUrl={foodItem.imgUrl}
+          // imgAlt={foodItem.imgAlt}
+          name={foodItem.mealName}
+          imgUrl={foodItem.mealImagePath}
+          imgAlt={foodItem.imgAlt}
+        />
+      );
 
-            if (index % 4 == 0) {
-                return (
-                    <React.Fragment>
-                        <div class="w-100"></div>
-                        {card}
-                    </React.Fragment>
-                );
-            } else {
-                return card;
-            }
-        });
-
+      if (index % 4 == 0) {
         return (
-            <div class="container mt-5 py-3">
-                <div class="row justify-content-center">
-                    {foodCards}
-                </div>
-            </div>
+          <React.Fragment>
+            <div class="w-100"></div>
+            {card}
+          </React.Fragment>
         );
-    }
+      } else {
+        return card;
+      }
+    });
+
+    return (
+      <div class="container mt-5 py-3">
+        <div class="row justify-content-center">{foodCards}</div>
+      </div>
+    );
+  }
 }
 
 class FoodCard extends Component {
-    render() {
-        return(
-            <span className="mr-3 ml-3 mt-3 col-md-4 col-sm-6 col-xl-2 listing">  
-                <img src={this.props.imgUrl} alt={this.props.imgAlt} style={{width: '200px', height: '200px'}} />
-                <p className="listingText">{this.props.name}</p>
-            </span>
-        );
-    }
+  render() {
+    const { name, imgUrl, imgAlt } = this.props;
+    return (
+      <span className="mr-3 ml-3 mt-3 col-md-4 col-sm-6 col-xl-2 listing">
+        <img
+          src={imgUrl}
+          alt={imgAlt}
+          style={{ width: "200px", height: "200px" }}
+        />
+        <p className="listingText">{name}</p>
+      </span>
+    );
+  }
 }
 
 export default CardList;
-
