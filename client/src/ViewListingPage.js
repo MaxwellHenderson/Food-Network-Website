ffbf5852521fad95de7a20b2ef91143b2aea282b
DNLuu <dn.luu03@gmail.com>
2020-02-04 18:26:28 -0800

    Implement basic listign view page.

--- client/src/ViewListingPage.js
+++ client/src/ViewListingPage.js
@@ -21,7 +21,8 @@ class ListingPage extends Component {
 
     render() {
         return (
-            <div id="viewListing" className="container h-100" style={{'margin': '100px 10%'}}>
+            // <div id="viewListing" className="container w-75" style={{'margin':'100px 10%'}}>
+            <div id="viewListing" className="container w-100" style={{'margin':'10vh auto'}}>
                 <div className="column border bg-light pt-2" id="photo-and-description">
                     <h2 id="title" className="mt-4 mb-2 pl-5">Shrimp Rice</h2>
                     <div className="border-top row mx-0"> 
@@ -45,34 +46,41 @@ class ListingPage extends Component {
                     <div id="tags" className="border-top px-4 row mx-0">
                         <div className="w-50">
                             <div className="my-4">
-                                <InputField labelName="Tags" 
-                                    placeHolder="Seafood, Spanish..." input={this.mealTagsInput} />
+                                <div className="ml-4 text-secondary">Tags</div>
+                                <div className="mx-2 border-top">
+                                    <div className="mx-1">Spanish    Seafood</div>
+                                </div>
                             </div>
                             <div className="my-4">
-                                <InputField labelName="Ingredients" 
-                                    placeHolder="" input={this.mealIngredientsInput} />
-                            </div>
+                                <div className="ml-4 text-secondary">Ingredients</div>
+                                    <div className="mx-2 border-top">
+                                        <div className="mx-1">Shrimp Rice Basil</div>
+                                        <div className="mx-1">Vegetable Oil Sausage</div>
+                                    </div>
+                                </div>
                             <div className="my-4">
-                                <InputField labelName="Allergy" 
-                                    placeHolder="Peanuts, Milk..." input={this.mealAllergyInput} />
+                                <div className="ml-4 text-secondary">Allergies</div>
+                                <div className="mx-2 border-top">
+                                    <div className="mx-1">Shrimp</div>
+                                </div>
                             </div>
                         </div>
                         <div className="w-50 py-4">
-                            <div id="purchase-form" className="border w-50 mx-auto py-3">
-                                <div className="row mt-1 mx-0">
-                                    <div className="column">
-                                        <img src=""></img>
+                            <div id="purchase-form" className="border w-75 mx-auto py-2 bg-light" style={{zIndex: 1, position: 'relative', top: '-10vh'}}>
+                                <div className="row mt-1 mx-0 px-3 pb-2 align-content-between">
+                                    <div className="column w-50">
+                                        <img src="/imgs/profile_default.jpg" className="rounded-circle" style={{height: '40px'}}></img>
                                         <div>John Doe</div>
                                     </div>
-                                    <div className="column">
-
+                                    <div className="column w-50 align-self-center">
+                                        <div id="price" className="align-self-center">$4.20 per item</div>
                                     </div>
                                 </div>
                                 <div className="border-top column pt-2 px-2">
-                                    <div className="row mx-0 justify-content-end px-4">
-                                        <div className="border">
-                                            <div>Quantity</div>
-                                            <div>2</div>
+                                    <div className="row mx-0 justify-content-end px-2">
+                                        <div className="border py-1 px-2 w-75">
+                                            <div className="text-secondary" style={{fontSize: "11px"}}>Quantity</div>
+                                            <div className="ml-1" style={{fontSize: "18px"}}>2</div>
                                         </div>
                                     </div>
                                     <div className="row mx-0 mt-2">
