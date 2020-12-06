455ba1aa9c8561aca5f3f0cb8d745e5f1104b3be
Max <xthatonekid@gmail.com>
2020-12-05 16:09:23 -0800

    Updates

---
+++ precache-manifest.1aff6570a44b571d2bec419c293be6d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ae20a224b01fdc3e075002fb11e790",
+    "url": "/Food-Network-Website/index.html"
+  },
+  {
+    "revision": "3013fe92dc4de565753e",
+    "url": "/Food-Network-Website/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "6612b4cfd48acf9bd638",
+    "url": "/Food-Network-Website/static/css/main.42ae1e05.chunk.css"
+  },
+  {
+    "revision": "3013fe92dc4de565753e",
+    "url": "/Food-Network-Website/static/js/2.e6846aed.chunk.js"
+  },
+  {
+    "revision": "e4362ac39bc868c9c0c31750f797cbc0",
+    "url": "/Food-Network-Website/static/js/2.e6846aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6612b4cfd48acf9bd638",
+    "url": "/Food-Network-Website/static/js/main.03ad044a.chunk.js"
+  },
+  {
+    "revision": "660efb75c0bd24c93606",
+    "url": "/Food-Network-Website/static/js/runtime-main.6d1da62f.js"
+  }
+]);
